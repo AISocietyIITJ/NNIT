006a695fd199da08e3180f1bb1b419158188614a
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2023-07-28 22:01:34 +0530

    Create decoder.py

---
+++ model/decoder.py
@@ -0,0 +1,11 @@
+from torch import nn
+
+class Decoder(nn.module):
+  '''
+  DEtr style decoder with multi-head self-attention,multi-head cross-attention 
+  and feed-forward network
+  '''
+  def__init__(image_size,embedding_size,n_heads,patch_size):
+    super().__init__()
+  def forward(self,input):
+    pass
