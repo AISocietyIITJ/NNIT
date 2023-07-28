a5ef5ebeffe4da39a5854bf230cbde9784ce11d3
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2023-07-28 21:58:59 +0530

    Create encoder.py

---
+++ model/encoder.py
@@ -0,0 +1,11 @@
+from torch import nn
+
+class Encoder(nn.module):
+  '''
+  Vision Transformer style encoder which returns 
+  patch_level representations of the input image
+  '''
+  def__init__(image_size,embedding_size,n_heads,patch_size):
+    super().__init__()
+  def forward(self,input):
+    pass
