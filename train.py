5e2ac7238701a1d17d4c1201264d32c2ea81bd76
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2023-07-28 22:03:22 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,11 @@
+'''
+Code for Training the encoder-decoder architecture
+'''
+from model.encoder import Encoder
+from model.decoder import Decoder
+
+def train():
+  pass
+
+if(__name__ == "main"):
+  train()
